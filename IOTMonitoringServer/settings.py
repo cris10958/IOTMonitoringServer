3286d24e827fa5e364e3c7283ed82f038c374925
Laura Cristina Fonseca Gil <lc.fonsecag1@uniandes.edu.co>
2024-09-16 14:44:01 -0500

    Ajuste de variables

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "34.207.180.95","181.59.2.131"]
+ALLOWED_HOSTS = ["localhost", "54.82.225.221"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "3.87.103.133",  # Dirección IP de la base de datos
+        "HOST": "23.22.163.138",  # Dirección IP de la base de datos
         "PORT": "5432",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "3.88.134.64"
+MQTT_HOST = "54.174.238.42"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
