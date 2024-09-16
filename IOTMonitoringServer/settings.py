9313c17ba5a9bbb7bd50aef53439e1d472a15a99
Laura Cristina Fonseca Gil <lc.fonsecag1@uniandes.edu.co>
2024-09-15 19:17:48 -0500

    Actualizacion de variables

--- IOTMonitoringServer/settings.py
+++ IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "54.226.192.240"]
+ALLOWED_HOSTS = ["localhost", "3.81.36.171"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "54.196.104.217",  # Dirección IP de la base de datos
+        "HOST": "18.207.174.126",  # Dirección IP de la base de datos
         "PORT": "5432",  # Puerto de la base de datos
     }
 }
@@ -156,18 +156,18 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.172.50.77"
+MQTT_HOST = "34.201.166.2"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
 
 # Credenciales para el usuario suscriptor del bróker MQTT
-MQTT_USER = "admin2"
-MQTT_PASSWORD = "admin234567*"
+MQTT_USER = "admin"
+MQTT_PASSWORD = "Blue12345678*"
 
 # Credenciales para el usuario publicador del bróker MQTT
 MQTT_USER_PUB = "admin2"
-MQTT_PASSWORD_PUB = "admin234567*"
+MQTT_PASSWORD_PUB = "Blue12345678*"
 
 # Tópico a suscribir. "+/+/+/+/out" se suscribe únicamente a los
 # tópicos con forma <país>/<estado>/<ciudad>/<usuario>/out.
