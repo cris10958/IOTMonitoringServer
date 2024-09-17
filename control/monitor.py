e0e4bcd87e1a08f1f0f17cb1299a461130b6180a
Laura Cristina Fonseca Gil <lc.fonsecag1@uniandes.edu.co>
2024-09-16 20:43:58 -0500

    ajustes proyecto

--- control/monitor.py
+++ control/monitor.py
@@ -45,14 +45,13 @@ def analyze_data():
         city = item['station__location__city__name']
         user = item['station__user__username']
 
-        alert = True
         if item["check_value"] > max_value or item["check_value"] < min_value:
             alert = True
 
         if alert:
             message = "ALERT {} {} {}".format(variable, min_value, max_value)
             topic = '{}/{}/{}/{}/in'.format(country, state, city, user)
-            print(datetime.now(), "Sending alert to {} {} mensaje {}".format(topic, variable,message))
+            print(datetime.now(), "Sending alert to {} {} {}".format(topic, variable, message))
             client.publish(topic, message)
             alerts += 1
 
